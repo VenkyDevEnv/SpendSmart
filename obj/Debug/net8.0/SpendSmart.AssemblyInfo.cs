173c025343f5f6f18a94d85d697faff8a1d861dc
VenkyDevEnv <vpeddinti450@gmail.com>
2024-10-21 15:53:35 +0530

    Added Edit and Delete features

--- obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7abb1dfc05da128542cafd1d1ae98db476a21aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
