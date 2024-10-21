7abb1dfc05da128542cafd1d1ae98db476a21aa6
VenkyDevEnv <vpeddinti450@gmail.com>
2024-10-21 13:21:28 +0530

    Initial commit

---
+++ Views/Home/Expense.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpendSmart.Views.Home
+{
+    public class ExpenseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
